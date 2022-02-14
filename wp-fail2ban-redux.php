5b2bc0a49e58d867f826c361e7216829ada3bcf7
Brandon Allen <cnorris23@gmail.com>
2022-02-13 23:29:51 -0600

    Fix "Requires PHP" version

--- wp-fail2ban-redux.php
+++ wp-fail2ban-redux.php
@@ -9,7 +9,7 @@
  * Domain Path:       /languages
  * Version:           0.8.2
  * Requires at least: 4.9
- * Requires PHP:      5.8
+ * Requires PHP:      5.6
  * License:           GPLv2 or later
  * License URI:       https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  *
